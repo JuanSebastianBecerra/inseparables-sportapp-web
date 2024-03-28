ef5e688113267f117a2606823e030de27b5f6f94
Juan Sebastian Becerra Bautista <j.becerrab@uniandes.edu.co>
2024-03-27 22:15:30 -0500

    feat: agregar componentes para manejar la autenticacion del usuario

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AutenticacionComponent } from './autenticacion/autenticacion.component';
+import { DesconocidoComponent } from './comunes/componentes/desconocido/desconocido.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: AutenticacionComponent },
+  { path: 'autenticacion', component: AutenticacionComponent },
+  { path: '**', component: DesconocidoComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/autenticacion/autenticacion.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import {MatGridListModule} from '@angular/material/grid-list';
+
+@Component({
+  selector: 'app-autenticacion',
+  templateUrl: './autenticacion.component.html',
+  styleUrls: ['./autenticacion.component.css'],
+  standalone: true,
+  imports: [MatGridListModule]
+})
+export class AutenticacionComponent {
+
+}
