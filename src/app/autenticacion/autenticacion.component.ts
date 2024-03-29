85d27d42f1916f1a640921f64a755dbda1eb4cdc
Juan Sebastian Becerra Bautista <j.becerrab@uniandes.edu.co>
2024-03-28 21:09:16 -0500

    feat: agregar bootstrap y maquetacion de pantalla autenticacion

--- src/app/autenticacion/autenticacion.component.ts
+++ src/app/autenticacion/autenticacion.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import {MatGridListModule} from '@angular/material/grid-list';
+import {MatCardModule} from '@angular/material/card';
 
 @Component({
   selector: 'app-autenticacion',
   templateUrl: './autenticacion.component.html',
   styleUrls: ['./autenticacion.component.css'],
   standalone: true,
-  imports: [MatGridListModule]
+  imports: [MatGridListModule, MatCardModule]
 })
 export class AutenticacionComponent {
 
