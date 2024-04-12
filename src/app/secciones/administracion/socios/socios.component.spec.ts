00bbb2fa038bbe551514fee8d4503c4943a7171a
JuanSebastianB <j.becerrab@uniandes.edu.co>
2024-04-11 22:32:20 -0500

    fix: pruebas unitarias menu

--- src/app/secciones/administracion/socios/socios.component.spec.ts
+++ src/app/secciones/administracion/socios/socios.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SociosComponent } from './socios.component';
+import { MenuComponent } from 'src/app/comunes/componentes/menu/menu.component';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('SociosComponent', () => {
   let component: SociosComponent;
@@ -8,7 +10,8 @@ describe('SociosComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [SociosComponent]
+      imports:[RouterTestingModule],
+      declarations: [SociosComponent, MenuComponent]
     });
     fixture = TestBed.createComponent(SociosComponent);
     component = fixture.componentInstance;
