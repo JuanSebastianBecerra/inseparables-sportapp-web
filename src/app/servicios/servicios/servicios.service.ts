98a9864c590596d7230c15b5d39a2e8a517410d8
monicabajonerodcastro <m.bajonero@uniandes.edu.co>
2024-04-14 11:57:04 -0500

    Update servicios.service.ts

--- src/app/servicios/servicios/servicios.service.ts
+++ src/app/servicios/servicios/servicios.service.ts
@@ -13,11 +13,8 @@ export class ServiciosService {
   constructor(private http: HttpClient) { }
 
   registrar_servicio(bodyRequest:any): Observable<any> {
-    /*const headers = new HttpHeaders({
-      'Authorization': `Bearer ${sessionStorage.getItem('token')}`
-    })*/
     const headers = new HttpHeaders({
-      'Authorization': `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3MTMwNzA2NTgsImlhdCI6MTcxMzA2OTc1OCwic3ViIjoicGVkcm8ucGVyZXoifQ.fnTJjaSKlEuolbkOYZW-l6PfVXLl8-DiwiOLfol0APs`
+      'Authorization': `Bearer ${sessionStorage.getItem('token')}`
     })
     return this.http.post<any>(this.serviciossUrl, bodyRequest, {observe: 'response', headers: headers});
   }
