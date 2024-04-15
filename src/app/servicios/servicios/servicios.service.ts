4735d43a39e3e67f9b938cb2aa160e53b6460f59
monicabajonerodcastro <m.bajonero@uniandes.edu.co>
2024-04-14 20:39:04 -0500

    Token de creación de servicios

--- src/app/servicios/servicios/servicios.service.ts
+++ src/app/servicios/servicios/servicios.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
 import { Observable } from 'rxjs';
+import { CookieService } from 'ngx-cookie-service';
 
 @Injectable({
   providedIn: 'root'
@@ -10,11 +11,11 @@ export class ServiciosService {
 
   private serviciossUrl = environment.baseUrlAdministracion + '/producto_servicio';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private cookieService: CookieService) { }
 
   registrar_servicio(bodyRequest:any): Observable<any> {
     const headers = new HttpHeaders({
-      'Authorization': `Bearer ${sessionStorage.getItem('token')}`
+      'Authorization': `Bearer ${this.cookieService.get("token")}`
     })
     return this.http.post<any>(this.serviciossUrl, bodyRequest, {observe: 'response', headers: headers});
   }
