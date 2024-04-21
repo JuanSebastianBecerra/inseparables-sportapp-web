d46c8f4b5379aaff510ff71d01ad248f0ff682e5
Nicolas_Gomez_Barbosa <n.gomezb2@uniandes.edu.co>
2024-04-20 22:22:14 -0500

    SPR-86 SPR-86-asociacion-de-entrenador-personalizado

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
     baseUrlPersonas: 'http://localhost:8080/personas',
-    baseUrlAdministracion: 'http://localhost:5001/administracion'
+    baseUrlAdministracion: 'http://localhost:8080/administracion'
 };
