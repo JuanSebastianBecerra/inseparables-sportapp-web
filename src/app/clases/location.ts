5515849142d9d7977714e3406030ef9780167618
monicabajonerodcastro <m.bajonero@uniandes.edu.co>
2024-05-03 21:18:37 -0500

    SPR-60: Consultar eventos

--- src/app/clases/location.ts
+++ src/app/clases/location.ts
@@ -2,14 +2,14 @@ export class UbicacionMaps{
     id: string
     direccion: string
     ubicacionLatitud: string
-    ubicacionLongitus: string
+    ubicacionLongitud: string
     nombre: string
 
-    constructor(id: string, direccion: string, ubicacionLatitud: string, ubicacionLongitus: string, nombre: string){
+    constructor(id: string, direccion: string, ubicacionLatitud: string, ubicacionLongitud: string, nombre: string){
         this.id = id
         this.direccion = direccion
         this.ubicacionLatitud = ubicacionLatitud
-        this.ubicacionLongitus = ubicacionLongitus
+        this.ubicacionLongitud = ubicacionLongitud
         this.nombre = nombre
     }
 
---
+++ src/app/secciones/eventos/listar-eventos/listar-eventos.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ListarEventosComponent } from './listar-eventos.component';
+
+describe('ListarEventosComponent', () => {
+  let component: ListarEventosComponent;
+  let fixture: ComponentFixture<ListarEventosComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [ListarEventosComponent]
+    });
+    fixture = TestBed.createComponent(ListarEventosComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
