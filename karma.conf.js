63424c248d6f720093488e9c572ad09e75a468f4
Nicolas_Gomez_Barbosa <n.gomezb2@uniandes.edu.co>
2024-05-03 22:52:14 -0500

    Se agregan pruebas pendientes

--- karma.conf.js
+++ karma.conf.js
@@ -36,8 +36,8 @@ module.exports = function (config) {
       check: {
         global: {
           statements: 80,
-          branches: 65,
-          functions: 75,
+          branches: 70,
+          functions: 80,
           lines: 80
         }
       }
