fca3703b774d568a6a1df1989bba0a0c56e534f2
neztoring <n.pereze@uniandes.edu.co>
2024-05-19 11:15:08 -0500

    Update agenda.component.spec.ts
    ajuste import

--- src/app/secciones/agenda/agenda.component.spec.ts
+++ src/app/secciones/agenda/agenda.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AgendaComponent } from './agenda.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('AgendaComponent', () => {
   let component: AgendaComponent;
@@ -8,7 +9,8 @@ describe('AgendaComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [AgendaComponent]
+      declarations: [AgendaComponent],
+      imports: [HttpClientTestingModule]
     });
     fixture = TestBed.createComponent(AgendaComponent);
     component = fixture.componentInstance;
